db9a333a694dc6a6af4352fdd85ea225e94d25b8
Igor Fedkov <igor.fedkov@gmail.com>
2021-01-24 02:11:41 +0200

    Задание 2 - Телефонная книга

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import { v4 as uuidv4 } from 'uuid';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import PhoneBook from './components/phoneBook'
+
+class App extends Component {
+  static propTypes = {
+    contacts: PropTypes.arrayOf(PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string
+    })),
+    filter: PropTypes.string
+  }
+
+  state = {
+    contacts: [
+      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+    ],
+    filter: ''
+  }
+
+  onInputChange = (event) => {
+    const { name, value } = event.target;
+		this.setState(
+      { [name]: value }
+    )
+  }
+  
+  addContact = ({ name, number }) => {
+    const usedContactName = this.state.contacts.find(contact => contact.name === name);
+    if (usedContactName !== undefined) {
+      alert(`${name} is already in contacts.`);
+      return;
+    }
+
+    const contact = {
+      id: uuidv4(),
+      name,
+      number
+    }
+    
+    this.setState(state => {
+      return {
+        contacts: [...state.contacts, contact]
+      }
+    })
+  };
+
+  deleteContact = (id => {
+    this.setState(state => {
+      return {contacts: state.contacts.filter(contact => contact.id !== id)}
+    })
+  })
+
+  
+
+  render() {
+    const { contacts, name, number, filter } = this.state;
+
+    return (
+      <div className="App">
+        <PhoneBook
+          name={name}
+          number={number}
+          addContact={this.addContact}
+          onInputChange={this.onInputChange}
+          contacts={contacts}
+          filter={filter}
+          onDeleteContact={this.deleteContact}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
